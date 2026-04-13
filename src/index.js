4ed8e6edbe1381e0fec9126407103c703d09cc1c
Abhay-Tewatia <abhaytewtia342@gmail.com>
2026-04-13 15:19:59 +0530

    building some controllers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import  dotenv from "dotenv"
 import {app} from "./app.js"
+import connectDB from "./db/index.js";
+
 
 dotenv.config();
+
 const PORT = process.env.PORT || 8000
-app.listen(PORT ,()=> {
-  console.log(`Server is running on port ${PORT}`);
+
+connectDB()
+.then(()=>{
+  app.listen(PORT,() => {
+    console.log(`Server is running on port: ${PORT}`);
+    
+  })
+})
+.catch((err) => {
+  console.log("Mongodb Connection error", err);
   
 })
